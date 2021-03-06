2d3dbf2dbc48b320bedebcecfd50e74789ce3dfa
olcay <olcaybayram@gmail.com>
2021-03-06 09:56:52 +0100

    End of day 2
    Includes
    - Player movement
    - Camera follow in boundaries
    - Parallax background

---
+++ Assets/CameraFollow2D.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2D : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    float timeOffset;
+
+    [SerializeField]
+    Vector2 posOffset;
+
+    [SerializeField]
+    float leftLimit;
+
+    [SerializeField]
+    float rightLimit;
+
+    [SerializeField]
+    float bottomLimit;
+
+    [SerializeField]
+    float topLimit;
+
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera start position
+        Vector3 startPos = transform.position;
+
+        //Players current position
+        Vector3 endPos = player.transform.position;
+        endPos.x += posOffset.x;
+        endPos.y += posOffset.y;
+        endPos.z = -10;
+
+        //this is lerp
+        transform.position = Vector3.Lerp(startPos, endPos, timeOffset * Time.deltaTime);
+
+        //this is smooth dampening
+        //transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffset);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit),
+            transform.position.z
+        );
+    }
+
+    private void OnDrawGizmos() {
+        // draw a box around our camera boundary
+        Gizmos.color = Color.red;
+
+        //top line
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+
+        //right line
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+
+        //bottom line
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+
+        //left line
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
+    }
+}
---
+++ Assets/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startPosX, startPosY;
+
+    public GameObject cam;
+    public float parallaxEffectX;
+    public float parallaxEffectY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float temp = cam.transform.position.x * (1 - parallaxEffectX);
+        float distX = cam.transform.position.x * parallaxEffectX;
+        float distY = cam.transform.position.y * parallaxEffectY;
+
+        transform.position = new Vector3(startPosX + distX, startPosY + distY, transform.position.z);
+
+        if (temp > startPosX + length) startPosX += length;
+        else if (temp < startPosX - length) startPosX -= length;
+    }
+}
